9410dbf2dc90657adbd1f76e34172b9f834bdd87
Toàn Hà Thanh <hathietthu2010@gmail.com>
2021-12-15 13:39:15 +0700

    chore: add ex4

---
+++ ws_unit_testing/lib/exercise4.ex
@@ -0,0 +1,9 @@
+defmodule WsUnitTesting.Exercise4 do
+  @doc """
+  Demo TDD
+  """
+  def search_province_by_keyword(keyword) do
+    # TODO: Define implementation here
+    raise "No province with key word #{keyword} found"
+  end
+end
---
+++ ws_unit_testing/test/exercise4_test.exs
@@ -0,0 +1,17 @@
+defmodule WsUnitTesting.Exercise4Test do
+  use ExUnit.Case
+  # import Mock
+  doctest WsUnitTesting
+
+  # Define your own test cases
+  # test "Should getting list provinces success" do
+  #   # Given
+  #   expected_output = 63
+
+  #   # When
+  #   {:ok, output} = WsUnitTesting.Exercise3.get_provinces()
+
+  #   # Then
+  #   assert length(output) == expected_output
+  # end
+end
